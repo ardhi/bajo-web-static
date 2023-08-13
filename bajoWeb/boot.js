5bb6d4c169eece89653f3a4714c849433fde80f6
Ardhi Lukianto <ardhi@lukianto.com>
2023-08-13 20:41:58 +0700

    Updates

--- bajoWeb/boot.js
+++ bajoWeb/boot.js
@@ -1,13 +1,17 @@
 import assets from '../lib/assets.js'
 import virtuals from '../lib/virtuals.js'
+import notFound from '../lib/not-found.js'
+import error from '../lib/error.js'
 
 async function boot () {
   const { getConfig } = this.bajo.helper
   const cfg = getConfig('bajoWebStatic')
   const prefix = cfg.prefix
   await this.bajoWeb.instance.register(async (ctx) => {
+    await error.call(this, ctx)
     await assets.call(this, ctx, prefix)
     await virtuals.call(this, ctx, prefix)
+    await notFound.call(this, ctx)
   }, { prefix })
 }
 
