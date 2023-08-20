8b27e09ec24656b345a0b0bec98d45832369507c
Ardhi Lukianto <ardhi@lukianto.com>
2023-08-20 20:46:11 +0700

    Refactor

--- bajoWeb/boot.js
+++ bajoWeb/boot.js
@@ -4,11 +4,14 @@ import notFound from '../lib/not-found.js'
 import error from '../lib/error.js'
 
 async function boot () {
-  const { getConfig } = this.bajo.helper
+  const { getConfig, importModule } = this.bajo.helper
   const cfg = getConfig('bajoWebStatic')
   const prefix = cfg.prefix
+  const cfgWeb = getConfig('bajoWeb', { full: true })
+  const routeHook = await importModule(`${cfgWeb.dir}/lib/route-hook.js`)
   await this.bajoWeb.instance.register(async (ctx) => {
-    this.bajoWebStatic.context = ctx
+    this.bajoWebStatic.instance = ctx
+    await routeHook.call(this, ctx, 'bajoWebStatic')
     await error.call(this, ctx)
     await asset.call(this, ctx, prefix)
     await virtual.call(this, ctx, prefix)
