3b3c12478ecb6794f3c46085abda004ea8bb29fc
Ardhi Lukianto <ardhi@lukianto.com>
2023-08-23 08:58:07 +0700

    Refactor

--- bajoWeb/boot.js
+++ bajoWeb/boot.js
@@ -3,20 +3,24 @@ import virtual from '../lib/virtual.js'
 import notFound from '../lib/not-found.js'
 import error from '../lib/error.js'
 
-async function boot () {
-  const { getConfig, importModule } = this.bajo.helper
-  const cfg = getConfig('bajoWebStatic')
-  const prefix = cfg.prefix
-  const cfgWeb = getConfig('bajoWeb', { full: true })
-  const routeHook = await importModule(`${cfgWeb.dir}/lib/route-hook.js`)
-  await this.bajoWeb.instance.register(async (ctx) => {
-    this.bajoWebStatic.instance = ctx
-    await routeHook.call(this, ctx, 'bajoWebStatic')
-    await error.call(this, ctx)
-    await asset.call(this, ctx, prefix)
-    await virtual.call(this, ctx, prefix)
-    await notFound.call(this, ctx)
-  }, { prefix })
+const boot = {
+  level: 10,
+  handler: async function boot () {
+    const { getConfig, importModule, runHook } = this.bajo.helper
+    const cfg = getConfig('bajoWebStatic')
+    const prefix = cfg.prefix
+    const cfgWeb = getConfig('bajoWeb', { full: true })
+    const routeHook = await importModule(`${cfgWeb.dir}/lib/route-hook.js`)
+    await this.bajoWeb.instance.register(async (ctx) => {
+      this.bajoWebStatic.instance = ctx
+      await runHook('bajoWebStatic:afterCreateContext', ctx)
+      await routeHook.call(this, 'bajoWebStatic')
+      await error.call(this, ctx)
+      await asset.call(this, ctx, prefix)
+      await virtual.call(this, ctx, prefix)
+      await notFound.call(this, ctx)
+    }, { prefix })
+  }
 }
 
 export default boot
