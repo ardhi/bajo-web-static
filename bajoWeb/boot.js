1042874ddeb13881a7c7f2524d41e5a5e2f90a70
Ardhi Lukianto <ardhi@lukianto.com>
2024-03-08 20:49:40 +0700

    Updates

--- bajoWeb/boot.js
+++ bajoWeb/boot.js
@@ -11,9 +11,17 @@ const boot = {
     const prefix = cfg.prefix
     const cfgWeb = getConfig('bajoWeb', { full: true })
     const routeHook = await importModule(`${cfgWeb.dir.pkg}/lib/route-hook.js`)
+    const handleCors = await importModule(`${cfgWeb.dir.pkg}/lib/handle-cors.js`)
+    const handleHelmet = await importModule(`${cfgWeb.dir.pkg}/lib/handle-helmet.js`)
+    const handleCompress = await importModule(`${cfgWeb.dir.pkg}/lib/handle-compress.js`)
+    const handleRateLimit = await importModule(`${cfgWeb.dir.pkg}/lib/handle-rate-limit.js`)
     await this.bajoWeb.instance.register(async (ctx) => {
       this.bajoWebStatic.instance = ctx
       await runHook('bajoWebStatic:afterCreateContext', ctx)
+      await handleRateLimit.call(this, ctx, cfg.rateLimit)
+      await handleCors.call(this, ctx, cfg.cors)
+      await handleHelmet.call(this, ctx, cfg.helmet)
+      await handleCompress.call(this, ctx, cfg.compress)
       await routeHook.call(this, 'bajoWebStatic')
       await error.call(this, ctx)
       await asset.call(this, ctx, prefix)
